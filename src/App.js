25b05a9fb9f9d2b1899217f75b647a0f7cf6c0ad
qyinm <qusseun@gmail.com>
2025-03-06 14:58:38 +0900

    modify app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.
+          Update Docker React test
         </a>
       </header>
     </div>
